5e36915cf48f01423811ac39e89338c9c92f4182
Jack Forrest <jack@jrforrest.net>
2017-08-22 16:33:45 -0600

    Corrects transactional fixture check for Rails 5
    In Rails 5 `ActiveSupport::TestCase` now uses `use_transactional_tests`
    rather than `use_transactional_fixtures` as its method to check whether
    transactional fixtures are being utilized for the current test.
    
    See: https://github.com/rails/rails/pull/19282

--- lib/slavery/transaction.rb
+++ lib/slavery/transaction.rb
@@ -1,16 +1,20 @@
 module Slavery
   class Transaction
+    # The methods on ActiveSupport::TestCase which can potentially be used
+    # to determine if transactional fixtures are enabled
+    TEST_CONFIG_METHODS = [
+      :use_transactional_tests,
+      :use_transactional_fixtures
+    ]
+
     class << self
       def base_depth
-        @base_depth ||= begin
-          testcase = ActiveSupport::TestCase
-          if defined?(testcase) &&
-              testcase.respond_to?(:use_transactional_fixtures) &&
-              testcase.try(:use_transactional_fixtures)
-            1
-          else
-            0
-          end
+        @base_depth ||= if defined?(ActiveSupport::TestCase) &&
+          TEST_CONFIG_METHODS.any? { |m| ActiveSupport::TestCase.try(m) }
+        then
+          1
+        else
+          0
         end
       end
     end
